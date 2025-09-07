663f592725ad4d0990466dc4503ca80883f30ec0
Rok Strnisa <rok.strnisa@gmail.com>
2025-09-07 22:39:18 +0100

    Format with prettier.

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    printWidth: 120,
+    semi: true,
+    singleQuote: false,
+    tabWidth: 4,
+    useTabs: false,
+    trailingComma: "all",
+};
+
+module.exports = config;
